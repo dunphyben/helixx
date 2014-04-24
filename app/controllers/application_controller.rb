b9d7db9e1d8cc1786e30f840baba0948d47d62f1
Josh Fowler <student@epicodus.com>
2014-04-24 10:19:11 -0700

    Add Devise Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,5 @@ class ApplicationController < ActionController::Base
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
+  #
 end
