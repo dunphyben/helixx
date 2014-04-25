63f9255b894163961dcff1439aaba97f60980516
Andrew Hallberg & Josh Paul <student@epicodus.com>
2014-04-25 13:59:54 -0700

    add the ability to see all users who voted for an idea, and how long ago

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,27 @@ belongs_to :idea
 
  validates :idea_id, uniqueness: { scope: :user_id }
 
+ def how_long
+    origin = self.created_at
+    age = Time.now - origin
+    if age < 3600
+      age = age/60
+      return "#{age.round(2)} minutes ago"
+    elsif age >= 3600 && age < 86400
+      age = age/3600
+      return "#{age.round(2)} hours ago"
+    elsif age >= 86400 && age <604800
+        age = age/86400
+        return "#{age.round(2)} days ago"
+    elsif age >= 604800 && age < 2419200
+      age = age/604800
+      return "#{age.round(2)} weeks ago"
+    elsif age >= 2419200 && age < 1.year
+      age = age/2419200
+      return "#{age.round(2)} months ago"
+    else
+      return "#{age.round(2)} more than a year ago"
+    end
+  end
+
 end
