81f4c0814508355ff74580f74e970f5ce3581888
Andrew Hallberg & Josh Paul <student@epicodus.com>
2014-04-25 11:31:33 -0700

    user login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   resources :tags
   resources :formats
   resources :videos
   resources :ideas
+  resources :votes
 
   get 'ideas/index/:id', to: 'ideas#index', via: :get
 
